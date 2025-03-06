9fa3b846b317a5f48d8e31fcc0a02c8b00a8b9bc
Denis Vasilenko <tuxtender@gmail.com>
2025-03-06 16:02:00 +0500

    Init WPF project

---
+++ LabTestBrowser.UI/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LabTestBrowser.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application { }
---
+++ LabTestBrowser.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿// Create a builder by specifying the application and main window.
+
+using LabTestBrowser.UI;
+
+var builder = WpfApplication<App, MainWindow>.CreateBuilder(args);
+
+// Build and run the application.
+var app = builder.Build();
+await app.RunAsync();
