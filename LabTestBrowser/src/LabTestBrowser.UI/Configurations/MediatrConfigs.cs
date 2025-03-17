1abe57dc8211c9e2b4938229664643f929adfe92
Denis Vasilenko <tuxtender@gmail.com>
2025-03-17 23:39:45 +0500

    Add configuration

---
+++ LabTestBrowser/src/LabTestBrowser.UI/Configurations/MediatrConfigs.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Ardalis.SharedKernel;
+using LabTestBrowser.Core.ContributorAggregate;
+using LabTestBrowser.UseCases.Contributors.Create;
+using MediatR;
+
+namespace LabTestBrowser.UI.Configurations;
+
+public static class MediatrConfigs
+{
+	public static IServiceCollection AddMediatrConfigs(this IServiceCollection services)
+	{
+		var mediatRAssemblies = new[]
+		{
+			Assembly.GetAssembly(typeof(Contributor)), // Core
+			Assembly.GetAssembly(typeof(CreateContributorCommand)) // UseCases
+		};
+
+		services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!))
+			.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
+			.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
+
+		return services;
+	}
+}
---
+++ LabTestBrowser/src/LabTestBrowser.UI/Configurations/OptionConfigs.cs
@@ -0,0 +1,24 @@
+﻿using LabTestBrowser.Infrastructure.Email;
+
+namespace LabTestBrowser.UI.Configurations;
+
+public static class OptionConfigs
+{
+	public static IServiceCollection AddOptionConfigs(this IServiceCollection services,
+		IConfiguration configuration,
+		ILogger logger,
+		WpfApplicationBuilder<App, MainWindow> builder)
+	{
+		// builder.Services.Configure<MySettings>(builder.Configuration.GetSection("MySettings"));
+		services.Configure<MailserverConfiguration>(configuration.GetSection("Mailserver"));
+
+		if (builder.Environment.IsDevelopment())
+		{
+		
+		}
+
+		logger.LogInformation("{Project} were configured", "Options");
+
+		return services;
+	}
+}
