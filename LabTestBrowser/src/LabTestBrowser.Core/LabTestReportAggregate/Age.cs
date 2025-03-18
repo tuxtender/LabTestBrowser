8fd37eb4c40755f23ef0a5d9ae2c33e276ef5b34
Denis Vasilenko <tuxtender@gmail.com>
2025-03-19 03:25:58 +0500

    Fix age value object

--- LabTestBrowser/src/LabTestBrowser.Core/LabTestReportAggregate/Age.cs
+++ LabTestBrowser/src/LabTestBrowser.Core/LabTestReportAggregate/Age.cs
@@ -2,18 +2,23 @@ namespace LabTestBrowser.Core.LabTestReportAggregate;
 
 public class Age : ValueObject
 {
-	public static Age None = new(null, null, null);
+	public static readonly Age None = new(null, null, null);
 
-	private Age(int? years, int? months, int? days) { }
+	private Age(int? years, int? months, int? days) 
+	{
+		Years = years;
+		Months = months;
+		Days = days;
+	}
 	public int? Years { get; private set; }
 	public int? Months { get; private set; }
 	public int? Days { get; private set; }
 
 	public static Result<Age> Create(int? years, int? months, int? days)
 	{
-		var isAgeAvailable = years.HasValue && months.HasValue && days.HasValue;
+		var isAgeAvailable = years.HasValue || months.HasValue || days.HasValue;
 
-		return !isAgeAvailable ? None : new Age(years, months, days);
+		return isAgeAvailable ? new Age(years, months, days) : None;
 	}
 
 	protected override IEnumerable<object> GetEqualityComponents()
