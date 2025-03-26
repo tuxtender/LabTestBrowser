6fae7939a40e9a915ffc66c830930cf47ece0876
Denis Vasilenko <tuxtender@gmail.com>
2025-03-27 04:51:00 +0500

    Add HL7 v231 segment key

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Hl7/Messaging/v231/MSH.cs
@@ -0,0 +1,25 @@
+﻿namespace LabTestBrowser.Infrastructure.Hl7.Messaging.v231;
+
+public static class MSH
+{
+	public const int FieldSeparator = 1;
+	public const int EncodingCharacters = 2;
+	public const int SendingApplication = 3;
+	public const int SendingFacility = 4;
+	public const int ReceivingApplication = 5;
+	public const int ReceivingFacility = 6;
+	public const int DateTimeOfMessage = 7;
+	public const int Security = 8;
+	public const int MessageType = 9;
+	public const int MessageControlId = 10;
+	public const int ProcessingId = 11;
+	public const int VersionId = 12;
+	public const int SequenceNumber = 13;
+	public const int ContinuationPointer = 14;
+	public const int AcceptAcknowledgmentType = 15;
+	public const int ApplicationAcknowledgmentType = 16;
+	public const int CountryCode = 17;
+	public const int CharacterSet = 18;
+	public const int PrincipalLanguageOfMessage = 19;
+	public const int AlternateCharacterSetHandlingScheme = 20;
+}
---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Hl7/Messaging/v231/OBX.cs
@@ -0,0 +1,22 @@
+﻿namespace LabTestBrowser.Infrastructure.Hl7.Messaging.v231;
+
+public static class OBX
+{
+	public const int Id = 1;
+	public const int ValueType = 2;
+	public const int ObservationIdentifier = 3;
+	public const int ObservationSubId = 4;
+	public const int ObservationValue = 5;
+	public const int Units = 6;
+	public const int ReferencesRange = 7;
+	public const int AbnormalFlags = 8;
+	public const int Probability = 9;
+	public const int NatureOfAbnormalTest = 10;
+	public const int ObservationResultStatus = 11;
+	public const int DateLastObsNormalValues = 12;
+	public const int UserDefinedAccessChecks = 13;
+	public const int DateTimeOfTheObservation = 14;
+	public const int ProducerId = 15;
+	public const int ResponsibleObserver = 16;
+	public const int ObservationMethod = 17;
+}
