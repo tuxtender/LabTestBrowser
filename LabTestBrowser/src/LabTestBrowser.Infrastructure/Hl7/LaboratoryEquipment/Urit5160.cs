cba7f751047f46eaf9304e505f35fb9849200591
Denis Vasilenko <tuxtender@gmail.com>
2025-03-27 04:53:22 +0500

    Add OBX keys to URIT tests

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Hl7/LaboratoryEquipment/Urit5160.cs
@@ -0,0 +1,20 @@
+﻿namespace LabTestBrowser.Infrastructure.Hl7.LaboratoryEquipment;
+
+public class Urit5160
+{
+	public const int WhiteBloodCell = 1;
+	public const int Lymphocyte = 2;
+	public const int MonocytePercent = 3;
+	public const int NeutrophilPercent = 4;
+	public const int EosinophilPercent = 5;
+	public const int BasophilPercent = 6;
+	public const int RedBloodCell = 7;
+	public const int Hemoglobin = 8;
+	public const int Hematocrit =9;
+	public const int MeanCorpuscularVolume = 10;
+	public const int MeanCorpuscularHemoglobin = 11;
+	public const int MeanCorpuscularHemoglobinConcentration = 12;
+	public const int RedBloodCellDistributionWidth = 13;
+	public const int Platelet = 14;
+	public const int MeanPlateletVolume = 15;
+}
