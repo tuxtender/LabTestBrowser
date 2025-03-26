3f28e14f827f5af17270f94ea1a5b3202f00f3d9
Denis Vasilenko <tuxtender@gmail.com>
2025-03-27 04:51:36 +0500

    Add unit of measurement enum

---
+++ LabTestBrowser/src/LabTestBrowser.Core/CompleteBloodCountAggregate/UnitOfMeasurement.cs
@@ -0,0 +1,11 @@
+﻿namespace LabTestBrowser.Core.CompleteBloodCountAggregate;
+
+public enum UnitOfMeasurement
+{
+	ConcentrationGl = 0, //	10^9/L
+	ConcentrationTl, //	10^12/L
+	Percent, //		%
+	GramPerLiter, // g/L
+	Fl, //	10^-15L
+	Pg //	10^-12 g
+}
