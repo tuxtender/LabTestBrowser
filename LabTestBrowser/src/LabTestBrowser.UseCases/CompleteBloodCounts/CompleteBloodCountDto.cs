5688f7492f8332545d651caa00e03a2bf5ce75fb
Denis Vasilenko <tuxtender@gmail.com>
2025-03-31 20:34:47 +0500

    Add CBC DTO

---
+++ LabTestBrowser/src/LabTestBrowser.UseCases/CompleteBloodCounts/CompleteBloodCountDto.cs
@@ -0,0 +1,22 @@
+﻿namespace LabTestBrowser.UseCases.CompleteBloodCounts;
+
+public record CompleteBloodCountDto
+{
+	public string? ExternalId { get; init; }
+	public DateTime ObservationDateTime { get; init; }
+	public string? WhiteBloodCell { get; init; }
+	public string? LymphocytePercent { get; init; }
+	public string? MonocytePercent { get; init; }
+	public string? NeutrophilPercent { get; init; }
+	public string? EosinophilPercent { get; init; }
+	public string? BasophilPercent { get; init; }
+	public string? RedBloodCell { get; init; }
+	public string? Hemoglobin { get; init; }
+	public string? Hematocrit { get; init; }
+	public string? MeanCorpuscularVolume { get; init; }
+	public string? MeanCorpuscularHemoglobin { get; init; }
+	public string? MeanCorpuscularHemoglobinConcentration { get; init; }
+	public string? RedBloodCellDistributionWidth { get; init; }
+	public string? Platelet { get; init; }
+	public string? MeanPlateletVolume { get; init; }
+}
