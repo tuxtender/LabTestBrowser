2bc06b8eed1c579821b4f790f3f0a2c3ffc66197
Denis Vasilenko <tuxtender@gmail.com>
2025-04-09 16:26:54 +0500

    Edit LabTestReportTemplate's field

--- LabTestBrowser/src/LabTestBrowser.Core/LabTestReportTemplateAggregate/LabTestReportTemplate.cs
+++ LabTestBrowser/src/LabTestBrowser.Core/LabTestReportTemplateAggregate/LabTestReportTemplate.cs
@@ -1,10 +1,10 @@
 ﻿namespace LabTestBrowser.Core.LabTestReportTemplateAggregate;
 
-public class LabTestReportTemplate( string facility, string tradeName, int animalId, string title, string path) : EntityBase, IAggregateRoot
+public class LabTestReportTemplate( string facility, string tradeName, string animal, string title, string path) : EntityBase, IAggregateRoot
 {
 	public string Facility { get; private set; } = facility;
 	public string TradeName { get; private set; } = tradeName;
-	public int AnimalId { get; private set; } = animalId;
+	public string Animal { get; private set; } = animal;
 	public string Title { get; private set; } = title;
 	public string Path { get; private set; } = path;
 }
