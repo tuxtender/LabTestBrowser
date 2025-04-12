1ccf2583865826909929f7f24feb137525c27508
Denis Vasilenko <tuxtender@gmail.com>
2025-04-13 03:24:11 +0500

    Change export service in the export use case

--- LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Export/ExportLabTestReportHandler.cs
+++ LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Export/ExportLabTestReportHandler.cs
@@ -1,13 +1,13 @@
 ﻿namespace LabTestBrowser.UseCases.LabTestReports.Export;
 
-public class ExportLabTestReportHandler(ISpreadSheetExportService _exportService) : ICommandHandler<ExportLabTestReportCommand, Result>
+public class ExportLabTestReportHandler(IExportService _exportService) : ICommandHandler<ExportLabTestReportCommand, Result>
 {
 	public async Task<Result> Handle(ExportLabTestReportCommand request, CancellationToken cancellationToken)
 	{
 		//TODO: Exception handling location
 
 		var exportTasks = request.LabTestReportTemplateIds
-			.Select(templateId => _exportService.Export(templateId, request.LabTestReportId));
+			.Select(templateId => _exportService.ExportAsync(request.LabTestReportId, templateId));
 		
 		await Task.WhenAll(exportTasks);
 		
