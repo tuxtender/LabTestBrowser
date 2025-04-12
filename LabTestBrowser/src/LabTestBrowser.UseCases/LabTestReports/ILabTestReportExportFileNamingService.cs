099885908063b074daed02f4930569c940f5a198
Denis Vasilenko <tuxtender@gmail.com>
2025-04-13 03:16:41 +0500

    Add export naming

---
+++ LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/ILabTestReportExportFileNamingService.cs
@@ -0,0 +1,6 @@
+﻿namespace LabTestBrowser.UseCases.LabTestReports;
+
+public interface ILabTestReportExportFileNamingService
+{
+	Task<string> GetExportFilenameAsync(int labTestReportId, int labTestReportTemplateId);
+}
