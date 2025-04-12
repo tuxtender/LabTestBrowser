3aa33fa45a9f03819499865a9677878d38e58ace
Denis Vasilenko <tuxtender@gmail.com>
2025-04-13 03:14:05 +0500

    Add template service resolver

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Export/ITemplateEngineResolver.cs
@@ -0,0 +1,8 @@
+﻿using LabTestBrowser.Core.LabTestReportTemplateAggregate;
+
+namespace LabTestBrowser.Infrastructure.Export;
+
+public interface ITemplateEngineResolver
+{
+	IFileTemplateEngine ResolveByFileFormat(TemplateFileFormat templateFileFormat);
+}
