8aa9d2d2267a6cca86809dee22235cf7cf47b22f
Denis Vasilenko <tuxtender@gmail.com>
2025-04-14 17:57:32 +0500

    Implement template engine resolver

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Export/TemplateEngineResolver.cs
@@ -0,0 +1,26 @@
+﻿using LabTestBrowser.Core.LabTestReportTemplateAggregate;
+
+namespace LabTestBrowser.Infrastructure.Export;
+
+public class TemplateEngineResolver(IExcelTemplateEngine excelTemplateEngine, IWordTemplateEngine wordTemplateEngine)
+	: ITemplateEngineResolver
+{
+	private readonly Dictionary<TemplateFileFormat, IFileTemplateEngine> _templateEngines = new()
+	{
+		{
+			TemplateFileFormat.Excel, excelTemplateEngine
+		},
+		{
+			TemplateFileFormat.Word, wordTemplateEngine
+		}
+	};
+
+	public IFileTemplateEngine ResolveByFileFormat(TemplateFileFormat templateFileFormat)
+	{
+		if (_templateEngines.TryGetValue(templateFileFormat, out var templateEngine))
+			return templateEngine;
+
+		//TODO: Add custom exception
+		throw new Exception($"Template engine not found for file format {templateFileFormat}");
+	}
+}
