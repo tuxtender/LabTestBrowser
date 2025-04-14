652c698d745d17d0761dc2788f8b86e1df9423ff
Denis Vasilenko <tuxtender@gmail.com>
2025-04-14 17:57:59 +0500

    Implement word template engine

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Export/WordTemplateEngine.cs
@@ -0,0 +1,26 @@
+﻿using NPOI.XWPF.UserModel;
+
+namespace LabTestBrowser.Infrastructure.Export;
+
+public class WordTemplateEngine : IWordTemplateEngine
+{
+	private const string TokenPattern = "{{{{{0}}}}}";
+
+	public Task<MemoryStream> RenderAsync(FileStream fileStream, Dictionary<string, string> tokens)
+	{
+		var doc = new XWPFDocument(fileStream);
+
+		foreach (var (token, value) in tokens)
+		{
+			var templateToken = string.Format(TokenPattern, token);
+			doc.FindAndReplaceText(templateToken, value);
+		}
+
+		var memoryStream = new MemoryStream();
+		doc.Write(memoryStream);
+		doc.Close();
+		memoryStream.Position = 0;
+
+		return Task.FromResult(memoryStream);
+	}
+}
