4870cb4ea7d65f2c9aa3c6375c89d6a9eaebcd18
Denis Vasilenko <tuxtender@gmail.com>
2025-04-14 21:34:57 +0500

    Convert DTO to CompleteBloodCount

---
+++ LabTestBrowser/src/LabTestBrowser.UseCases/CompleteBloodCounts/CompleteBloodCountExtensions.cs
@@ -0,0 +1,31 @@
+﻿using LabTestBrowser.Core.CompleteBloodCountAggregate;
+
+namespace LabTestBrowser.UseCases.CompleteBloodCounts;
+
+public static class CompleteBloodCountExtensions
+{
+	public static CompleteBloodCountDto ConvertToCompleteBloodCountDto(this CompleteBloodCount cbc)
+	{
+		return new CompleteBloodCountDto
+		{
+			Id = cbc.Id,
+			ExternalId = cbc.ExternalId,
+			ObservationDateTime = cbc.ObservationDateTime,
+			WhiteBloodCell = cbc.WhiteBloodCell?.Value,
+			LymphocytePercent = cbc.LymphocytePercent?.Value,
+			MonocytePercent = cbc.MonocytePercent?.Value,
+			NeutrophilPercent = cbc.NeutrophilPercent?.Value,
+			EosinophilPercent = cbc.EosinophilPercent?.Value,
+			BasophilPercent = cbc.BasophilPercent?.Value,
+			RedBloodCell = cbc.RedBloodCell?.Value,
+			Hemoglobin = cbc.Hemoglobin?.Value,
+			Hematocrit = cbc.Hematocrit?.Value,
+			MeanCorpuscularVolume = cbc.MeanCorpuscularVolume?.Value,
+			MeanCorpuscularHemoglobin = cbc.MeanCorpuscularHemoglobin?.Value,
+			MeanCorpuscularHemoglobinConcentration = cbc.MeanCorpuscularHemoglobinConcentration?.Value,
+			RedBloodCellDistributionWidth = cbc.RedBloodCellDistributionWidth?.Value,
+			Platelet = cbc.Platelet?.Value,
+			MeanPlateletVolume = cbc.Platelet?.Value,
+		};
+	}
+}
