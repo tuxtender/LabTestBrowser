00e947d9d242e26c40d7f52b9d62ed568323af08
Denis Vasilenko <tuxtender@gmail.com>
2025-04-18 02:47:07 +0500

    Migrate AccessionNumber unique constraint

---
+++ LabTestBrowser/src/LabTestBrowser.Infrastructure/Data/Migrations/20250417214122_AddAccessionNumberUniqueConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabTestBrowser.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccessionNumberUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CompleteBloodCounts_AccessionNumber_SequenceNumber_AccessionNumber_Date",
+                table: "CompleteBloodCounts",
+                columns: new[] { "AccessionNumber_SequenceNumber", "AccessionNumber_Date" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CompleteBloodCounts_AccessionNumber_SequenceNumber_AccessionNumber_Date",
+                table: "CompleteBloodCounts");
+        }
+    }
+}
