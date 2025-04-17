b0ae3f7b9e6e3d5a723ab56033cd5347cdfe6b29
Denis Vasilenko <tuxtender@gmail.com>
2025-04-17 16:17:44 +0500

    Add the accession number
    That identifies batch of lab test

---
+++ LabTestBrowser/src/LabTestBrowser.Core/CompleteBloodCountAggregate/AccessionNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace LabTestBrowser.Core.CompleteBloodCountAggregate;
+
+public class AccessionNumber : ValueObject
+{
+	private AccessionNumber(int sequenceNumber, DateOnly date)
+	{
+		SequenceNumber = sequenceNumber;
+		Date = date;
+	}
+
+	public int SequenceNumber { get; private set; }
+	public DateOnly Date { get; private set; }
+
+	public static Result<AccessionNumber> Create(int sequenceNumber, DateOnly date)
+	{
+		if (sequenceNumber < 1)
+			return Result.Error();
+
+		return new AccessionNumber(sequenceNumber, date);
+	}
+
+	protected override IEnumerable<object> GetEqualityComponents()
+	{
+		yield return SequenceNumber;
+		yield return Date;
+	}
+}
