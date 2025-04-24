f7614a544f55c22a005c1897467b4bb02bb3ad7f
Denis Vasilenko <tuxtender@gmail.com>
2025-04-25 03:54:41 +0500

    Refactor UpdateLabTestReportHandler

--- LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Update/UpdateLabTestReportHandler.cs
+++ LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Update/UpdateLabTestReportHandler.cs
@@ -1,35 +1,36 @@
 ﻿using LabTestBrowser.Core.LabTestReportAggregate;
+using Microsoft.Extensions.Logging;
 
 namespace LabTestBrowser.UseCases.LabTestReports.Update;
 
-public class UpdateLabTestReportHandler(IRepository<LabTestReport> _repository)
+public class UpdateLabTestReportHandler(IRepository<LabTestReport> _repository, ILogger<UpdateLabTestReportHandler> _logger)
 	: ICommandHandler<UpdateLabTestReportCommand, Result<LabTestReportDto>>
 {
 	public async Task<Result<LabTestReportDto>> Handle(UpdateLabTestReportCommand request, CancellationToken cancellationToken)
 	{
 		var labTestReport = await _repository.GetByIdAsync(request.Id, cancellationToken);
-
 		if (labTestReport == null)
+		{
+			_logger.LogInformation("LabTestReport id: {labTestReportId} not found", request.Id);
 			return Result.NotFound();
+		}
 
 		var specimenCollectionCenter = SpecimenCollectionCenter.Create(request.Facility, request.TradeName!);
-
 		if (!specimenCollectionCenter.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(specimenCollectionCenter.ValidationErrors);
 
 		var age = Age.Create(request.AgeInYears, request.AgeInMonths, request.AgeInDays);
-
 		if (!age.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(age.ValidationErrors);
 
 		var patient = Patient.Create(request.Animal, age, request.PetOwner, request.Nickname, request.Category, request.Breed);
-
 		if (!patient.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(patient.ValidationErrors);
 
 		labTestReport.UpdateSpecimenCollectionCenter(specimenCollectionCenter);
 		labTestReport.UpdatePatient(patient);
 		await _repository.UpdateAsync(labTestReport, cancellationToken);
+		_logger.LogInformation("Updated LabTestReport id: {labTestReportId}", request.Id);
 
 		return labTestReport.ConvertToLabTestReportDto();
 	}
