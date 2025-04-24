6feefaf91d75c924d58b8e3d8926b75607a37182
Denis Vasilenko <tuxtender@gmail.com>
2025-04-25 03:41:08 +0500

    Refactor CreateLabTestReportHandler

--- LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Create/CreateLabTestReportHandler.cs
+++ LabTestBrowser/src/LabTestBrowser.UseCases/LabTestReports/Create/CreateLabTestReportHandler.cs
@@ -1,43 +1,42 @@
 ﻿using LabTestBrowser.Core.CompleteBloodCountAggregate;
 using LabTestBrowser.Core.LabTestReportAggregate;
 using LabTestBrowser.Core.LabTestReportAggregate.Specifications;
+using Microsoft.Extensions.Logging;
 
 namespace LabTestBrowser.UseCases.LabTestReports.Create;
 
-public class CreateLabTestReportHandler(IRepository<LabTestReport> _repository)
+public class CreateLabTestReportHandler(IRepository<LabTestReport> _repository, ILogger<CreateLabTestReportHandler> _logger)
 	: ICommandHandler<CreateLabTestReportCommand, Result<LabTestReportDto>>
 {
 	public async Task<Result<LabTestReportDto>> Handle(CreateLabTestReportCommand request, CancellationToken cancellationToken)
 	{
 		var accessionNumber = AccessionNumber.Create(request.SequenceNumber, request.Date);
-
 		if (!accessionNumber.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(accessionNumber.ValidationErrors);
 
 		var spec = new LabTestReportByAccessionNumberSpec(accessionNumber);
 		var labTestReport = await _repository.FirstOrDefaultAsync(spec, cancellationToken);
-
 		if (labTestReport != null)
-			return Result.Error();
+		{
+			_logger.LogInformation("LabTestReport id: {labTestReportId} is already created", labTestReport.Id);
+			return Result.NotFound();
+		}
 
 		var specimenCollectionCenter = SpecimenCollectionCenter.Create(request.Facility, request.TradeName!);
-
 		if (!specimenCollectionCenter.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(specimenCollectionCenter.ValidationErrors);
 
 		var age = Age.Create(request.AgeInYears, request.AgeInMonths, request.AgeInDays);
-
 		if (!age.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(age.ValidationErrors);
 
 		var patient = Patient.Create(request.Animal, age, request.PetOwner, request.Nickname, request.Category, request.Breed);
-
 		if (!patient.IsSuccess)
-			return Result.Error();
+			return Result.Invalid(patient.ValidationErrors);
 
 		labTestReport = new LabTestReport(accessionNumber, specimenCollectionCenter, patient);
-
 		await _repository.AddAsync(labTestReport, cancellationToken);
+		_logger.LogInformation("Created LabTestReport for accession number: {sequenceNumber} {date}", request.SequenceNumber, request.Date);
 
 		return labTestReport.ConvertToLabTestReportDto();
 	}
