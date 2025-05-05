a2268781a4dd7f7891980675d200fe7bd91ca7fa
Denis Vasilenko <tuxtender@gmail.com>
2025-05-05 17:32:24 +0500

    Add validator

---
+++ LabTestBrowser/src/LabTestBrowser.UI/PositiveIntValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Markup;
+
+namespace LabTestBrowser.UI;
+
+public class PositiveIntValidator: MarkupExtension
+{
+	public override object ProvideValue(IServiceProvider serviceProvider)
+	{
+		return new Func<string, string>(text =>
+		{
+			return (int.TryParse(text, out int value) && value > 0
+				? null
+				: "Введите число больше 0")!;
+		});
+	}
+}
