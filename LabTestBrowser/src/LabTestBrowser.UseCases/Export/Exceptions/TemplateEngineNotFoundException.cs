d7aa541b8078cbcc9e75de18341d727395e9894b
Denis Vasilenko <tuxtender@gmail.com>
2025-05-13 18:35:21 +0500

    Add export exceptions

---
+++ LabTestBrowser/src/LabTestBrowser.UseCases/Export/Exceptions/TemplateEngineNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace LabTestBrowser.UseCases.Export.Exceptions;
+
+public class TemplateEngineNotFoundException : Exception
+{
+	public TemplateEngineNotFoundException()
+		: base("Template engine not registered") { }
+
+	public TemplateEngineNotFoundException(Exception inner)
+		: base("Template engine not registered", inner) { }
+}
---
+++ LabTestBrowser/src/LabTestBrowser.UseCases/Export/Exceptions/TemplateFileNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿namespace LabTestBrowser.UseCases.Export.Exceptions;
+
+public class TemplateFileNotFoundException : IOException
+{
+	public TemplateFileNotFoundException(string filePath)
+	{
+		FilePath = filePath;
+	}
+
+	public TemplateFileNotFoundException(string filePath, string message)
+		: base(message)
+	{
+		FilePath = filePath;
+	}
+
+	public TemplateFileNotFoundException(string filePath, string message, Exception inner)
+		: base(message, inner)
+	{
+		FilePath = filePath;
+	}
+
+	public string FilePath { get; }
+}
