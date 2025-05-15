50d1a97fdd913202186d701d09fc3366dc7ec601
Denis Vasilenko <tuxtender@gmail.com>
2025-05-16 03:48:39 +0500

    Specify TargetFramework per project

--- LabTestBrowser/src/LabTestBrowser.Desktop/Configurations/LoggerConfigs.cs
+++ LabTestBrowser/src/LabTestBrowser.Desktop/Configurations/LoggerConfigs.cs
@@ -1,5 +1,6 @@
 ﻿using LabTestBrowser.Desktop.Navigation;
 using Serilog;
+using Serilog.Settings.Configuration;
 
 namespace LabTestBrowser.Desktop.Configurations;
 
@@ -7,8 +8,37 @@ public static class LoggerConfigs
 {
 	public static WpfApplicationBuilder<App, MainWindow> AddLoggerConfigs(this WpfApplicationBuilder<App, MainWindow> builder)
 	{
-		builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+		// builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
 
+		var configuration = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json", optional: false)
+			.Build();
+
+		var readerOptions = new ConfigurationReaderOptions(
+			typeof(ConsoleLoggerConfigurationExtensions).Assembly,
+			typeof(FileLoggerConfigurationExtensions).Assembly
+		);
+
+		builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+		{
+			loggerConfiguration
+				.ReadFrom.Configuration(configuration, readerOptions);
+		});
+		
+		// builder.Host.UseSerilog((_, config) =>
+		// 	new LoggerConfiguration()
+		// 		.ReadFrom.Configuration(configuration, readerOptions)
+		// 		.CreateLogger()
+		// );
+
+		
+		// builder.Host.UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
+		// 	.ReadFrom.Configuration(hostingContext.Configuration)
+		// 	.Enrich.FromLogContext()
+		// 	.WriteTo.File(
+		// 		@"Logs\log.txt", 
+		// 		rollingInterval: RollingInterval.Day));
+		
 		return builder;
 	}
 }
