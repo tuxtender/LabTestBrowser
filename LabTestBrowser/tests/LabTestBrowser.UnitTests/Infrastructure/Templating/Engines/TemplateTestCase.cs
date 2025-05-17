b7ff8fcadd06d0bfdb0f232c6b18601a38d8e39e
Denis Vasilenko <tuxtender@gmail.com>
2025-05-17 22:21:32 +0500

    Test TextTemplateEngine

---
+++ LabTestBrowser/tests/LabTestBrowser.UnitTests/Infrastructure/Templating/Engines/TemplateTestCase.cs
@@ -0,0 +1,9 @@
+﻿namespace LabTestBrowser.UnitTests.Infrastructure.Templating.Engines;
+
+public record TemplateTestCase
+{
+	public required string Template { get; init; }
+	public required string Expected { get; init; }
+	public string[] Keys { get; init; } = [];
+	public string[] Values { get; init; } = [];
+}
