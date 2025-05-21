fcc2611bea408e72ff0979c94ef59dc4f3b3cea7
Denis Vasilenko <tuxtender@gmail.com>
2025-05-22 04:18:53 +0500

    Remove redundant namespace aliases

--- LabTestBrowser/src/LabTestBrowser.Desktop/Configurations/PresentationConfigs.cs
+++ LabTestBrowser/src/LabTestBrowser.Desktop/Configurations/PresentationConfigs.cs
@@ -1,13 +1,11 @@
 ﻿using LabTestBrowser.Desktop.Dialogs;
+using LabTestBrowser.Desktop.LabResult;
+using LabTestBrowser.Desktop.LabResult.CompleteBloodCount;
 using LabTestBrowser.Desktop.LabResult.LabRequisition;
+using LabTestBrowser.Desktop.LabResult.ReportExportDialog;
 using LabTestBrowser.Desktop.Navigation;
 using LabTestBrowser.Desktop.Notification;
 using LabTestBrowser.Desktop.SearchLabResult;
-using CompleteBloodCountViewModel = LabTestBrowser.Desktop.LabResult.CompleteBloodCount.CompleteBloodCountViewModel;
-using LabResultViewModel = LabTestBrowser.Desktop.LabResult.LabResultViewModel;
-using MainWindowViewModel = LabTestBrowser.Desktop.Navigation.MainWindowViewModel;
-using ReportExportDialogViewModel = LabTestBrowser.Desktop.LabResult.ReportExportDialog.ReportExportDialogViewModel;
-using StatusBarViewModel = LabTestBrowser.Desktop.Notification.StatusBarViewModel;
 
 namespace LabTestBrowser.Desktop.Configurations;
 
